72434d6a1a1de5256fc6f8a2be04be2cb3516a91
akuma-code <akumapl86@gmail.com>
2023-10-07 21:27:16 +0300

    sync

--- src/mobXStore/Stores.ts
+++ src/mobXStore/Stores.ts
@@ -1,5 +1,6 @@
 import { makeAutoObservable } from "mobx"
 import { _ID, _log } from "../Helpers/HelpersFns"
+import { DTO_FunctionType, DTO_TemplateObj } from "../Interfaces/MathActionsTypes"
 
 export interface IDTO_SimpleAction_v1 {
     callback: (...args: number[]) => number
@@ -47,3 +48,34 @@ export default class mbxActionStore<T>{
         return this.store.map(n => n)
     }
 }
+
+
+export class mbxFnNode<T extends (...args: any[]) => {}> {
+    fn: T
+    args?: Parameters<T>
+    constructor(public func: T, args?: Parameters<T>) {
+        this.fn = func
+        this.args = args
+    }
+
+    getDTO_Template(...args: typeof this.args[]) {
+
+        if (!args) return {
+            fn: this.fn
+        } as DTO_FunctionType<T>
+        const output = this.fn(...args)
+        return {
+            args, fn: this.fn, output
+        } as DTO_TemplateObj<T>
+    }
+}
+
+export class mbxFnStore<T>{
+
+}
+
+
+function multi5(a: number) { return a * 5 }
+const node = new mbxFnNode(multi5, [4])
+
+const t = node.getDTO_Template()

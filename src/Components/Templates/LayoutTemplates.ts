7db9d4d548b19b541b072ccff76a9004f054aecf
akuma-code <akumapl86@gmail.com>
2023-10-31 17:55:25 +0300

    layout template

---
+++ src/Components/Templates/LayoutTemplates.ts
@@ -0,0 +1,19 @@
+import { TypeSelector } from "../../ActionComponents/ActionTypes/Types"
+import { createOutputTableData } from "../FlexForm/OutputTable"
+
+type tmpl_form = Pick<TypeSelector<'nets'>, 'fields' | 'initstate'>
+
+export function tmpl_layout() {
+    const buttons = [
+        'nets', 'offset5'
+    ]
+
+    const form: tmpl_form = {
+        fields: ['width', 'height'],
+        initstate: { width: 500, height: 800 }
+    }
+
+    const out = createOutputTableData<'nets'>({ width: 500, height: 800 }, { skf: { h: 100, w: 100 }, simple: { h: 50, w: 50 } })
+
+    return { buttons, form, out }
+}

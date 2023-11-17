6971f5629dcd7a352cf8f7ce850e17e67268c250
akuma-code <akumapl86@gmail.com>
2023-11-18 01:49:35 +0300

    sync

--- src/ActionComponents/ActionTypes/FnProperties.ts
+++ src/ActionComponents/ActionTypes/FnProperties.ts
@@ -1,3 +1,6 @@
+import { ISizeFull } from "../../Interfaces/CommonTypes";
+import { ANYobj } from "../../Interfaces/MathActionsTypes";
+
 export type FnPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? K : never }[keyof T]
 
 export type FnProperties<T> = Pick<T, FnPropertyNames<T>>;
@@ -7,4 +10,24 @@ export type NonFnPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? ne
 export type NonFnProperties<T> = Pick<T, NonFnPropertyNames<T>>;
 
 export type StringifyProps<T> = { [K in keyof T]: T[K] extends string ? T : T[K] extends number ? string | number : string }
-export type StringifyProps_2<T> = { [K in keyof T]: T[K] extends string ? T : T[K] extends number ? string : string }
+
+
+export type StringifyProps_2<T> = {
+    [K in keyof T]: T[K] extends string ?
+    T :
+    T[K] extends number ?
+    "" :
+    T
+}
+
+// export type GetFieldsArray<T extends ANYobj> =
+//     T extends T[infer Key extends keyof T] ?
+//     { fields: readonly Key[] }
+//     :
+//     { fields: readonly (keyof T)[] }
+
+export type GetFieldsArray<T extends ANYobj> = { [Field in keyof T]: T[Field] extends number | string ? { fields: readonly [Field[]] } : never }
+
+
+
+

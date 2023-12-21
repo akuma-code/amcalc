0c58dea152efaba7e0fbe120c74ff1848d9c35b2
akuma-code <akumapl86@gmail.com>
2023-12-21 18:48:56 +0300

    form

--- src/Components/FlexForm/SillForm.tsx
+++ src/Components/FlexForm/SillForm.tsx
@@ -4,46 +4,55 @@ import { _ID } from '../../Helpers/HelpersFns';
 import { useStoresContext } from '../../Hooks/useStoresContext';
 import { A_Sill, _ArgsMaker } from '../../Interfaces/CommonTypes';
 import { Button, Input } from '@mui/material';
+import { observer } from 'mobx-react-lite';
 
 type SillFormProps = {}
 type FormValues = {
     sill: A_Sill[]
 }
-const SillForm = (props: SillFormProps) => {
+const SillForm: React.FC<SillFormProps> = observer(() => {
     const { SillStore } = useStoresContext()
     const {
         register, control, handleSubmit, formState: { errors }, reset
     } = useForm<FormValues, FormValues>({
         defaultValues: {
-            sill: [{ L: 1100, B: 180, count: 1 }]
+            sill: [{ count: 1 }]
         }
     })
 
     const { fields, append, remove } = useFieldArray({ name: 'sill', control: control })
     const onSubmitFn: SubmitHandler<FormValues> = (payload) => {
 
-        payload.sill.forEach(a => {
+        payload.sill.map(a => {
             SillStore && SillStore.add(a)
+            return a
         });
         console.log('FormValues: ', payload)
-        control._reset()
-        return payload
+        reset()
+        // return payload
     }
     return (
-        <Form control={control}
+        <Form control={control} name='sill_form' id={'sill_form'}
             onSubmit={({ data }) => onSubmitFn(data)}>
             {
                 fields.map((f, idx) =>
                     <div key={f.id} className='flex flex-col w-fit bg-yellow-200'>
-                        <Input {...register(`sill.${idx}.L` as const, { required: true, shouldUnregister: true })} name='L' placeholder='L' />
-                        <Input {...register(`sill.${idx}.B` as const, { required: true, shouldUnregister: true })} name='B' placeholder='B' />
-                        <Input {...register(`sill.${idx}.count` as const, { required: true, shouldUnregister: true })} name='count' placeholder='counter' />
+                        <Input type='number'{...register(`sill.${idx}.L` as const, { required: true, shouldUnregister: true, })} name='L' placeholder='L' />
+                        <Input type='number'{...register(`sill.${idx}.B` as const, { required: true, shouldUnregister: true, })} name='B' placeholder='B' />
+                        <Input type='number'{...register(`sill.${idx}.count` as const, { required: true, shouldUnregister: true, })} name='count' placeholder='counter' />
                     </div>
                 )
             }
-            <Button type='submit' variant='contained' color='error'>Submit</Button>
+            <Button type='submit'
+                form='sill_form'
+                variant='contained'
+                color='success'
+                sx={{ margin: 1 }}
+            >
+                SUBMIT
+            </Button>
         </Form>
     )
-}
+})
 
 export default SillForm

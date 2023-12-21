74a6f24f51315c97cfa49feb61cae43d70aee8c6
akuma-code <akumapl86@gmail.com>
2023-12-21 19:14:40 +0300

    sync

--- src/Components/FlexForm/SillForm.tsx
+++ src/Components/FlexForm/SillForm.tsx
@@ -1,45 +1,49 @@
 import React, { BaseSyntheticEvent, SyntheticEvent } from 'react'
-import { useForm, useFieldArray, useWatch, Control, Form, SubmitHandler } from "react-hook-form";
+import { useForm, useFieldArray, useWatch, Control, Form as HForm, SubmitHandler } from "react-hook-form";
 import { _ID } from '../../Helpers/HelpersFns';
 import { useStoresContext } from '../../Hooks/useStoresContext';
 import { A_Sill, _ArgsMaker } from '../../Interfaces/CommonTypes';
 import { Button, Input } from '@mui/material';
 import { observer } from 'mobx-react-lite';
+import { spawn } from 'child_process';
+import { DevTool } from '@hookform/devtools';
+import { Form } from 'react-router-dom';
 
 type SillFormProps = {}
 type FormValues = {
     sill: A_Sill[]
 }
 const SillForm: React.FC<SillFormProps> = observer(() => {
-    const { SillStore } = useStoresContext()
+    const { SillStore, ViewConfig } = useStoresContext()
     const {
-        register, control, handleSubmit, formState: { errors }, reset
-    } = useForm<FormValues, FormValues>({
+        register, control, handleSubmit, formState: { errors, }, reset
+    } = useForm<FormValues>({
         defaultValues: {
-            sill: [{ count: 1 }]
+            sill: []
         }
     })
 
-    const { fields, append, remove } = useFieldArray({ name: 'sill', control: control })
-    const onSubmitFn: SubmitHandler<FormValues> = (payload) => {
+    const { fields, append, remove, } = useFieldArray({ name: 'sill', control: control })
+    const onSubmitFn: SubmitHandler<FormValues> = (data, e) => {
 
-        payload.sill.map(a => {
+        data.sill.forEach(a => {
             SillStore && SillStore.add(a)
-            return a
+
         });
-        console.log('FormValues: ', payload)
-        reset()
+        console.log('FormValues: ', data.sill)
+        // reset()
         // return payload
     }
     return (
-        <Form control={control} name='sill_form' id={'sill_form'}
-            onSubmit={({ data }) => onSubmitFn(data)}>
+        <HForm id={'sill_form'}
+        // onSubmit={({data})=>onSubmitFn(data)}
+        >
             {
                 fields.map((f, idx) =>
                     <div key={f.id} className='flex flex-col w-fit bg-yellow-200'>
-                        <Input type='number'{...register(`sill.${idx}.L` as const, { required: true, shouldUnregister: true, })} name='L' placeholder='L' />
-                        <Input type='number'{...register(`sill.${idx}.B` as const, { required: true, shouldUnregister: true, })} name='B' placeholder='B' />
-                        <Input type='number'{...register(`sill.${idx}.count` as const, { required: true, shouldUnregister: true, })} name='count' placeholder='counter' />
+                        <Input {...register(`sill.${idx}.L` as const, { required: true, })} name='L' placeholder='L' />
+                        <Input {...register(`sill.${idx}.B` as const,)} name='B' placeholder='B' />
+                        <Input {...register(`sill.${idx}.count` as const,)} name='count' placeholder='counter' />
                     </div>
                 )
             }
@@ -51,7 +55,8 @@ const SillForm: React.FC<SillFormProps> = observer(() => {
             >
                 SUBMIT
             </Button>
-        </Form>
+            {<DevTool control={control} />}
+        </HForm>
     )
 })
 

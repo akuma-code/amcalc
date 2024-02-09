b0b1bec9a005c3d2d8d6256978f188ebcf78d87e
akuma-code <akumapl86@gmail.com>
2024-02-10 02:02:35 +0300

    sync

--- src/Components/Pages/Router/DrawerImage/FrameView.tsx
+++ src/Components/Pages/Router/DrawerImage/FrameView.tsx
@@ -11,7 +11,7 @@ import { drawframe } from '../../../../Models/Drower/DrawerFns';
 import { _DRAWPATH } from '../../../../Models/Drower/DrawPaths';
 import { TSidesArray } from '../../../../Interfaces/Enums';
 import { toJS } from 'mobx';
-import { _SizeF, _isArr, _ss } from '../../../../Helpers/HelpersFns';
+import { _SizeF, _isArr, _p, _ss } from '../../../../Helpers/HelpersFns';
 import { JsxElement } from 'typescript';
 import { IconButton } from '../../../UI/IconButton';
 import Icons from '../../../Icons/SvgIcons';
@@ -21,6 +21,7 @@ import { observer } from 'mobx-react-lite';
 import { useQuery } from 'react-query';
 import MemoFrameFFsvg from '../../../../Assets/FrameFFsvg';
 import { Rulers } from './Rulers';
+import { useFrameData } from "../../../../Hooks/useFrameData";
 type FrameViewProps = {}
 
 
@@ -59,13 +60,13 @@ export const FrameView = observer((props: FrameViewProps) => {
         const rects = nodes.map((n, i) => {
             const { coords, _id: id, } = n
             const [anc, e] = coords
-            const [_w, _h] = [Math.abs(anc.x - e.x), Math.abs(anc.y - e.y),]
-            const { x } = anc
+            const { x, y } = anc
+            const [_w, _h] = [Math.abs(x - e.x), Math.abs(y - e.y),]
             return <rect
-                x={ i > 1 ? x + 20 : x }
-                y={ anc.y }
+                x={ i >= 1 ? x + 20 : x }
+                y={ y }
                 key={ id }
-                width={ anc.x + _w }
+                width={ _w }
                 height={ _h }
                 fill={ isSelected(id) ? '#da6e6e' : '#5baac2' }
                 stroke='black'
@@ -73,9 +74,12 @@ export const FrameView = observer((props: FrameViewProps) => {
             />
         }
         )
+
+
         const { width, height } = currentFrame.size
-        const vb = `0 0 ${width} ${height}`
-        return <svg viewBox={ vb } preserveAspectRatio='xMinYMin meet' width={ width }>{ rects }</svg>
+        const { x, y } = currentFrame.pos
+        const vb = `0 0 ${width + x} ${height + y}`
+        return <svg viewBox={ vb } preserveAspectRatio='xMinYMin meet' width={ width } height={ height }>{ rects }</svg>
     }, [selected?._id, nodes, currentFrame, handleClickOnNode])
 
     return currentFrame ?
@@ -177,6 +181,9 @@ export const FrameView = observer((props: FrameViewProps) => {
             </NavLink>
         </Box>
 })
+
+FrameView.displayName = `_____Frame View Page`
+
 type FrameCanvasProps = {
     _viewbox?: string
     _scale?: number | string
--- src/Hooks/useFrameData.ts
+++ src/Hooks/useFrameData.ts
@@ -1,2 +1,100 @@
-import { _Point } from "../Helpers/HelpersFns";
+import { useEffect, useState } from "react";
+import { _Point, _SizeF, _getcoords, _log, _p, _ss } from "../Helpers/HelpersFns";
+
+import { FrameNodeWithSides } from "../Models/FrameComponent/FrameFactory/FrameNodeWithSides";
 import { _TCoords } from "../Models/FrameComponent/StvState";
+
+
+
+type InitArgsType<T> = T extends [infer F, _Point]
+    ? F extends _SizeF
+    ? [_SizeF, _Point]
+    : F extends _Point
+    ? [_Point, _Point]
+    : T
+    : never
+
+type IUseFrameData = {
+    rama: { size: _SizeF, pos: _Point }
+    nodes: FrameNodeWithSides[]
+
+}
+
+export function useFrameData(data_rama: IUseFrameData['rama'], ...data_nodes: FrameNodeWithSides[]) {
+    const { size, pos } = data_rama
+
+    const frame_rama_control = useFrameRama(size, pos)
+    const nodes = useFrameNodes(frame_rama_control[0],
+        ...data_nodes,
+    )
+
+
+    return [frame_rama_control, nodes] as const
+
+}
+
+function useFrameRama(size: _SizeF = { width: 1000, height: 1000 }, pos: _Point = { x: 0, y: 0 }) {
+    const { width, height } = size
+    const { x, y } = pos
+    const [frame_rama, setRama] = useState({ size, pos })
+
+    useEffect(() => {
+
+        setRama(prev => ({ ...prev, size: _ss(width, height), pos: _p(x, y) }))
+
+
+
+    }, [height, x, y, width])
+
+    return [frame_rama, setRama] as const
+}
+
+const useFrameNodes = (rama: { size: _SizeF, pos: _Point }, ...rest: (FrameNodeWithSides | undefined)[]) => {
+    const { size, pos } = rama
+    const nc = _getcoords(size, pos)
+    let node = rest[0]
+    if (node === undefined) {
+        node = new FrameNodeWithSides()
+        node
+            .setNodeSize(size)
+            .setNodeCoords([...nc])
+
+        _log(node)
+        return [node] as const
+    } else {
+        const nodes = rest.map(n => n?.setNodeSize(_ss(size.width / 2, size.height))
+
+        )
+        // console.log('nodes', nodes)
+        return [...nodes] as const
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+type FrameNodeArgs = [_SizeF, _Point] | [_Point, _Point]
+function useFrameNodes_Bad(...args: [_SizeF, _Point][]) {
+    const nodes = args.map((arg) => {
+        arg.map(n => {
+            if ('width' in n) {
+                return { ...n }
+            } else return { ...n }
+        })
+        return [...arg]
+    }
+    )
+
+}
+
+function _isSizeOrPoint<T extends _SizeF | _Point>(arg: T): arg is T {
+    if ('x' in arg) return arg.x !== undefined
+    else return arg.width !== undefined
+}
